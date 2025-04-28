af68fc656c19d419d195ace486a354c3ed64502a
xyao7chen <xingyao7chen@gmail.com>
2025-04-29 00:51:20 +0200

    add file

---
+++ API/test_xy.py
@@ -0,0 +1,8 @@
+# A test for git push from Xingyao, will delete it after the test
+# pip install azure-ai-projects
+
+def hello():
+    print("hello from API(it's just a test from xingyao. will delete it later)")
+
+if __name__ == "__main__":
+    hello()
