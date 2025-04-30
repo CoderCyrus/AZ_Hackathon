043769f6df1d94bbb12983710db3563646d5ac73
xyao7chen <xingyao7chen@gmail.com>
2025-04-30 19:32:32 +0200

    add .bat files for llm call testing

---
+++ API/test_call1_llm.bat
@@ -0,0 +1,4 @@
+curl -X POST http://localhost:7071/api/HttpExample ^
+-H "Content-Type: application/json" ^
+-d "{\"input\": {\"Subject\": \"[Learn a song by guitar]\", \"TotalPeriodHours\": \"[10]hours\", \"Frequency\": \"[30]mins\", \"Style\": \"[Encouraging]\"}}"
+pause
--- API/test_xy.py
+++
@@ -1,8 +0,0 @@
-# A test for git push from Xingyao, will delete it after the test
-# pip install azure-ai-projects
-
-def hello():
-    print("hello from API(it's just a test from xingyao. will delete it later)")
-
-if __name__ == "__main__":
-    hello()
