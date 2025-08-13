f317f614af713c3b627098a7eba1c229fc03862c
CoderCyrus <cyrusbilibili@gmail.com>
2025-08-13 21:59:24 +0200

    clean

--- front/src/config/azure.ts
+++
@@ -1,5 +0,0 @@
-export const azureConfig = {
-  functionUrl: import.meta.env.VITE_AZURE_FUNCTION_URL || '',
-  blobUrl: import.meta.env.VITE_AZURE_BLOB_URL || '',
-  containerName: 'motivational-sentences'
-};
